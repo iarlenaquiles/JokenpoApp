eecb46999bb7453762224cd217c9652917c7a4b1
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-20 22:27:25 -0300

    Criando o método sorteia jogada

--- app/src/main/java/com/tecnologia/aquiles/jokenpo/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/jokenpo/MainActivity.java
@@ -3,8 +3,12 @@ package com.tecnologia.aquiles.jokenpo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     ImageButton botaoPedra;
     ImageButton botaoPapel;
     ImageButton botaoTesoura;
+
+    Animation some;
+    Animation aparece;
+
+    int jogada1 = 0;
+    int jogada2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +37,48 @@ public class MainActivity extends AppCompatActivity {
         botaoPapel = findViewById(R.id.botaoPapel);
         botaoTesoura = findViewById(R.id.botaoTesoura);
 
+        some = new AlphaAnimation(1, 0);
+        aparece = new AlphaAnimation(0, 1);
+
+        some.setDuration(1500);
+        aparece.setDuration(100);
+
+        some.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                jogador2.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                jogador2.setVisibility(View.INVISIBLE);
+                jogador2.startAnimation(aparece);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        aparece.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                sorteiaJogadaInimigo();
+                jogador2.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                verificaJogada();
+                jogador2.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     public void tocouBotao(View view){
@@ -34,14 +86,42 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case(R.id.botaoPedra):
                 jogador1.setImageResource(R.drawable.pedra);
+                jogada1 = 1;
                 break;
             case(R.id.botaoPapel):
                 jogador1.setImageResource(R.drawable.papel);
+                jogada1 = 2;
                 break;
             case(R.id.botaoTesoura):
                 jogador1.setImageResource(R.drawable.tesoura);
+                jogada1 = 3;
                 break;
         }
+        jogador2.setImageResource(R.drawable.interrogacao);
+        jogador2.startAnimation(some);
+
+    }
+
+    public void sorteiaJogadaInimigo() {
+        Random r = new Random();
+        int numRandom = r.nextInt(3);
+        switch (numRandom) {
+            case 0:
+                jogador2.setImageResource(R.drawable.pedra);
+                jogada2 = 1;
+                break;
+            case 1:
+                jogador2.setImageResource(R.drawable.papel);
+                jogada2 = 2;
+                break;
+            case 2:
+                jogador2.setImageResource(R.drawable.tesoura);
+                jogada2 = 3;
+                break;
+        }
+    }
+
+    public void verificaJogada() {
 
     }
 }
