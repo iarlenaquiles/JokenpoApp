1a3a1078bde2bffef2ead431ee26aef048f9101b
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-10-20 22:04:29 -0300

    Criando o método tocoubotao

--- app/src/main/java/com/tecnologia/aquiles/jokenpo/MainActivity.java
+++ app/src/main/java/com/tecnologia/aquiles/jokenpo/MainActivity.java
@@ -2,12 +2,34 @@ package com.tecnologia.aquiles.jokenpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView jogador1;
+    ImageView jogador2;
+
+    ImageButton botaoPedra;
+    ImageButton botaoPapel;
+    ImageButton botaoTesoura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogador1 = findViewById(R.id.jogador1);
+        jogador2 = findViewById(R.id.jodagor2);
+
+        botaoPedra = findViewById(R.id.botaoPedra);
+        botaoPapel = findViewById(R.id.botaoPapel);
+        botaoTesoura = findViewById(R.id.botaoTesoura);
+
+    }
+
+    public void tocouBotao(View view){
+
     }
 }
